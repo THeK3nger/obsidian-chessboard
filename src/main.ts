3524e3a99992afd7016cdbfc18a8087f279c431e
Davide Aversa <thek3nger@gmail.com>
2021-03-01 17:29:33 +0100

    refactor: improve rendering function

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,6 @@ import {
   MarkdownPostProcessorContext,
   MarkdownPreviewRenderer,
   Plugin,
-  PluginSettingTab,
-  Setting,
 } from "obsidian";
 import { SVGChessboard } from "./chessboardsvg/index";
 
@@ -16,38 +14,17 @@ export default class ObsidianChess extends Plugin {
   onInit() {}
 
   async onload() {
-    MarkdownPreviewRenderer.registerPostProcessor(ObsidianChess.postprocessor);
-
-    // This snippet of code is used to load pluging settings from disk (if any)
-    // and then add the setting tab in the Obsidian Settings panel.
-    // If your plugin does not use settings, you can delete these two lines.
-    // this.setting = (await this.loadData()) || {
-    //   someConfigData: 1,
-    //   anotherConfigData: "defaultValue",
-    // };
-    //this.addSettingTab(new MyPluginSettingsTab(this.app, this));
-  }
-
-  onunload() {
-    MarkdownPreviewRenderer.unregisterPostProcessor(
+    this.registerMarkdownCodeBlockProcessor(
+      "chessboard",
       ObsidianChess.postprocessor
     );
   }
 
-  static postprocessor: MarkdownPostProcessor = (
+  static postprocessor = (
+    source: string,
     el: HTMLElement,
     ctx: MarkdownPostProcessorContext
   ) => {
-    const blockToReplace = el.querySelector("pre");
-    console.log(blockToReplace);
-    if (!blockToReplace) return;
-
-    const chessBlock = blockToReplace.querySelector("code.language-chessboard");
-    console.log(chessBlock);
-    if (!chessBlock) return;
-
-    const source = chessBlock.textContent;
-
     const chessboard = SVGChessboard.fromFEN(source);
 
     const xmlns = "http://www.w3.org/2000/svg";
@@ -59,41 +36,6 @@ export default class ObsidianChess extends Plugin {
     block.setAttributeNS(null, "height", String(boxHeight));
     block.appendChild(chessboard.draw());
     block.style.display = "block";
-    el.replaceChild(block, blockToReplace);
+    el.appendChild(block);
   };
 }
-
-/**
- * This is a data class that contains your plugin configurations. You can edit it
- * as you wish by adding fields and all the data you need.
- */
-// interface MyPluginSettings {
-//   someConfigData: number;
-//   anotherConfigData: string;
-// }
-
-// class MyPluginSettingsTab extends PluginSettingTab {
-//   plugin: MyPlugin;
-
-//   constructor(app: App, plugin: MyPlugin) {
-//     super(app, plugin);
-//     this.plugin = plugin;
-//   }
-
-//   display(): void {
-//     const { containerEl } = this;
-//     const settings = this.plugin.setting;
-//     // This is just an example of a setting controll.
-//     new Setting(containerEl)
-//       .setName("Setting Name")
-//       .setDesc("Setting description")
-//       .addText((text) =>
-//         text.setValue(String(settings.someConfigData)).onChange((value) => {
-//           if (!isNaN(Number(value))) {
-//             settings.someConfigData = Number(value);
-//             this.plugin.saveData(settings);
-//           }
-//         })
-//       );
-//   }
-// }
